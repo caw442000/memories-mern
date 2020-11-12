8e3c7b233abf0f866a20982bb37cdfcabfb1809b
cedric-winbush <cawinbushjr@gmail.com>
2020-11-12 15:22:58 -0600

    adds createPost to server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+
+import { MONGODB } from './config.js';
+
+
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+
+mongoose.set('useFindAndModify', false);
+
