f71343d2137d395f0a5e1d4661663ef323daaa0d
cedric-winbush <cawinbushjr@gmail.com>
2020-11-14 14:08:17 -0600

    adds sytles for header

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,22 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
 
+import useStyles from './styles'
+
 import memories from "./assets/images/memories.png";
 
 import "./App.css";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className= {classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" />
+        <img className={classes.image} src={memories} alt="memories" height="60"/>
       </AppBar>
       <Grow in>
         <Container>
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import useStyles from './styles'
+
 
 const Post = () => {
   return (
