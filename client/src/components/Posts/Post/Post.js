6f85742088b30f663984c387efa24f4347f34e33
cedric-winbush <cawinbushjr@gmail.com>
2020-11-20 16:45:18 -0600

    adds background svg completes card display

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -35,8 +35,26 @@ const Post = ({ post }) => {
         </Button>
       </div>
       <div className={classes.details}>
-        
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
       </div>
+      <CardContent>
+        <Typography className={classes.title} variant="h5" gutterBottom>
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+            <ThumbUpAltIcon fontSize="small"/>
+            Like {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+            <DeleteIcon fontSize="small"/>
+            Delete
+        </Button>
+
+      </CardActions>
     </Card>
   );
 };
