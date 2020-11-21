d623b489621301baabd43cf6809a1870e5fa74e9
cedric-winbush <cawinbushjr@gmail.com>
2020-11-21 14:47:45 -0600

    adds update Post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,6 @@
 import PostMessage from '../models/postMessage.js';
+import mongoose from 'mongoose';
+
 
 
 export const getPosts = async (req, res) => {
@@ -23,5 +25,23 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
+
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  // _id is mongoose id
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).sent(`No post with that ${id}`)
+    
+  }
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  await PostMessage.findByIdAndUpdate(id, updatedPost, {new: true})
+  res.json(updatedPost)
 
 }
